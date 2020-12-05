16c5c52786f6ad2ef2135335b24690f9da19c2f0
ely <ely.gandara.c@gmail.com>
2020-12-04 23:32:39 -0700

    Functional code added with 3000~ episodes played

--- README.md
+++ README.md
@@ -1,8 +1,11 @@
 # pr3-atari-ai
-Proyecto #3 y entrega final de la materia Inteligencia Artificial. Desarrollo de un agente de Inteligencia Artificial que sea capaz de jugar un juego de Atari utilizando OpenAI Gym como entorno.
+Proyecto #3 y entrega final de la materia Inteligencia Artificial. Agente de Inteligencia Artificial que sea capaz de jugar un juego de Atari utilizando OpenAI Gym como entorno.
 
-## Aprendizaje forzado e implementación
-Inicialmente en el aprendizaje forzado (a diferencia del aprendizaje supervisado) se desconoce qué está bien o qué está mal de nuestra información, la única forma de saber esto al inicio es probando y ver si funciona. Se auto generan variables a traves de observaciones y retroalimentación.
+Para este proyecto se utilizió el repositorio de mrahtz (https://github.com/mrahtz/tensorflow-rl-pong) que está basado en la entrada del blog http://karpathy.github.io/2016/05/31/rl/ de Andrej Karpathy.
+Se entrenó la inteligencia artificial desde 0 para mejor comprensión del funcionamiento del aprendizaje reforzado.
+
+## Aprendizaje forzado - Introducción
+Inicialmente en el aprendizaje reforzado (a diferencia del aprendizaje supervisado) se desconoce qué está bien o qué está mal de nuestra información, la única forma de saber esto al inicio es probando y ver si funciona. Se auto generan variables a traves de observaciones y retroalimentación.
 
 Uno de los ejemplos que utiliza Soroush (thinkingparticle) para comprender mejor lo que es el aprendizaje forzado y sus implicaciones, nos pide que nos imaginemos un mundo imaginario donde somos gerentes de un nuevo departamento y no conocemos a nuestros empleados, no tenemos acceso a sus curriculum ni registros. No sabemos cual empleado es un diseñador, un programador, o del área de atención al cliente, etc.
 
@@ -13,12 +16,37 @@ El acercamiento a la solución de esta situación que propone es intentar y fall
 Agente: es aquel que está aprendiendo a ganar y adquiere beneficios para sí mismo (recompensas)
 Ambiente(environment): El mundo con el que el agente puede interactuar. El agente "ve" states y hace "acciones" de acuerdo a lo que ve.
 Recompensa(reward): El agente obtiene recompensas o castigos según sus acciones. 
+Policy gradient: Una de las clases de algoritmos de control más importantes. Es un acercamiento para resolver problemas de aprendizaje reforzado.
 
-## 
+## Policy Gradients 
 
-A systematic study on the Pong environment by Phon-Amnuaisuk suggested that roughly 10000 episodes of data is needed to generate an agent capable of achieving a win.
-https://arxiv.org/pdf/1807.08452.pdf
+En este algoritmo se usó una policy para ver que acciones llevan a recompensas altas para incrementar la probabilidad de que se repitan.
+El proposito del aprendizaje forzado es encontrar la estrategia de comportamiento mas optima para que el agente reciba las mejores recompensas. 
+
+# Stochastic policy 
+Estocástico: Sometido al azar y que es objeto de analisis estadistico. Se aplica a procesos, algoritmos y modelos en los que existe una secuencia cambiante de eventos analizables probabilísticamente a medida que pasa el tiempo.
+Toma ejemplos de acciones acciones y si esas acciones eventualmente ocurren y llevan a un buen desenlace son motivadas para que se repitan en el futuro, pero si esas acciones tomadas llevan aun mal resultando, son desmotivadas.
+
+
+
+# Vocabulario:
+
+    'Round': Un partida en la que un jugador gana un punto
+    'Episode': una serie de rounds que determina un juego "completado" (Usualmente cerca 20 mas o menos segun la misma decisi'on del juego)
+
+# Tiempo de entrenamiento
+
+    Toma cerca de 500 episodios ver si el agente ha mejorado o no
+    Toma cerca de 7000 episodios llegar a un estado en el que el agente gana la mitad y pierde la mitad de los round
+
+# Protocolo de entrenamiento
+
+La política 
+Training protocol. So here is how the training will work in detail. We will initialize the policy network with some W1, W2 and play 100 games of Pong (we call these policy “rollouts”). Lets assume that each game is made up of 200 frames so in total we’ve made 20,000 decisions for going UP or DOWN and for each one of these we know the parameter gradient, which tells us how we should change the parameters if we wanted to encourage that decision in that state in the future. All that remains now is to label every decision we’ve made as good or bad. For example suppose we won 12 games and lost 88. We’ll take all 200*12 = 2400 decisions we made in the winning games and do a positive update (filling in a +1.0 in the gradient for the sampled action, doing backprop, and parameter update encouraging the actions we picked in all those states). And we’ll take the other 200*88 = 17600 decisions we made in the losing games and do a negative update (discouraging whatever we did). And… that’s it. The network will now become slightly more likely to repeat actions that worked, and slightly less likely to repeat actions that didn’t work. Now we play another 100 games with our new, slightly improved policy and rinse and repeat.
+
 ## Fuentes y referencias
+https://github.com/mrahtz/tensorflow-rl-pong
+https://www.tensorflow.org/guide/upgrade
 https://gym.openai.com/envs/#atari
 https://github.com/thinkingparticle/deep_rl_pong_keras
-https://nbviewer.jupyter.org/github/thinkingparticle/deep_rl_pong_keras/blob/master/reinforcement_learning_pong_keras_policy_gradients.ipynb
+
--- atarigym.py
+++ atarigym.py
@@ -1,323 +1,140 @@
-import gym
+#!/usr/bin/env python
+"""
+Train a Pong AI using policy gradient-based reinforcement learning.
+
+Based on Andrej Karpathy's "Deep Reinforcement Learning: Pong from Pixels"
+http://karpathy.github.io/2016/05/31/rl/
+and the associated code
+# https://gist.github.com/karpathy/a4166c7fe253700972fcbc77e4ea32c5
+"""
+
+from __future__ import print_function
+
+import argparse
+import pickle
 import numpy as np
-# limitar velocidad 1/2
-import time
-import keras # used 2.1.16 with tensorflow 1.8.0/
+import gym
+
+from policy_network import Network
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--hidden_layer_size', type=int, default=200)
+parser.add_argument('--learning_rate', type=float, default=0.0005)
+parser.add_argument('--batch_size_episodes', type=int, default=1)
+parser.add_argument('--checkpoint_every_n_episodes', type=int, default=10)
+parser.add_argument('--load_checkpoint', action='store_true')
+parser.add_argument('--discount_factor', type=int, default=0.99)
+parser.add_argument('--render', action='store_true')
+args = parser.parse_args()
+
+# Action values to send to gym environment to move paddle up/down
+UP_ACTION = 2
+DOWN_ACTION = 3
+# Mapping from action values to outputs from the policy network
+action_dict = {DOWN_ACTION: 0, UP_ACTION: 1}
 
 
+# From Andrej's code
+def prepro(I):
+    """ prepro 210x160x3 uint8 frame into 6400 (80x80) 1D float vector """
+    I = I[35:195]  # crop
+    I = I[::2, ::2, 0]  # downsample by factor of 2
+    I[I == 144] = 0  # erase background (background type 1)
+    I[I == 109] = 0  # erase background (background type 2)
+    I[I != 0] = 1  # everything else (paddles, ball) just set to 1
+    return I.astype(np.float).ravel()
 
-import matplotlib.pyplot as plt
-%matplotlib inline
+
+def discount_rewards(rewards, discount_factor):
+    discounted_rewards = np.zeros_like(rewards)
+    for t in range(len(rewards)):
+        discounted_reward_sum = 0
+        discount = 1
+        for k in range(t, len(rewards)):
+            discounted_reward_sum += rewards[k] * discount
+            discount *= discount_factor
+            if rewards[k] != 0:
+                # Don't count rewards from subsequent rounds
+                break
+        discounted_rewards[t] = discounted_reward_sum
+    return discounted_rewards
 
 env = gym.make('Pong-v0')
-observation = env.reset()
-observation.shape
 
-for _ in range(1000):
-    # env.render()
+network = Network(
+    args.hidden_layer_size, args.learning_rate, checkpoints_dir='checkpoints')
+if args.load_checkpoint:
+    network.load_checkpoint()
 
-    # # Limitar velocidad 2/2
-    # time.sleep(0.05)
+batch_state_action_reward_tuples = []
+smoothed_reward = None
+episode_n = 1
 
-    # i take some steps to see middle game scene    
-    # in pong game,
-    # 0 is numeric action to stay at same place 
-    # 2 is numeric action to move paddle up in game
-    # 3 is numeric action to move paddle down in game
-    
-    observation, reward, done, info = env.step(env.action_space.sample()) # take a random action
-    
-    
-    new_observation, reward, done, info = env.step(2)
-    plt.imshow(observation)
+while True:
+    print("Starting episode %d" % episode_n)
 
-    # 
+    episode_done = False
+    episode_reward_sum = 0
 
-    # so the definition of system in reinforcement learning method is simple:
+    round_n = 1
 
-    # - state is the screen of game.
-    # - action is going up or down
+    last_observation = env.reset()
+    last_observation = prepro(last_observation)
+    action = env.action_space.sample()
+    observation, _, _, _ = env.step(action)
+    observation = prepro(observation)
+    n_steps = 1
 
-    # we could also define 3 actions (go up, go down, stay still) but here we just use the 2 mentioned above
+    while not episode_done:
+        if args.render:
+            env.render()
 
-def preprocess_frames(new_frame,last_frame):
-# inputs are 2 numpy 2d arrays
-n_frame = new_frame.astype(np.int32)
-n_frame[(n_frame==144)|(n_frame==109)]=0 # remove backgound colors
-l_frame = last_frame.astype(np.int32)
-l_frame[(l_frame==144)|(l_frame==109)]=0 # remove backgound colors
-diff = n_frame - l_frame
-# crop top and bot 
-diff = diff[35:195]
-# down sample 
-diff=diff[::2,::2]
-# convert to grayscale
-diff = diff[:,:,0] * 299. / 1000 + diff[:,:,1] * 587. / 1000 + diff[:,:,2] * 114. / 1000
-# rescale numbers between 0 and 1
-max_val =diff.max() if diff.max()> abs(diff.min()) else abs(diff.min())
-if max_val != 0:
-    diff=diff/max_val
-return diff
+        observation_delta = observation - last_observation
+        last_observation = observation
+        # up_probability = network.forward_pass(observation_delta)[0]
+        up_probability = network.forward_pass(observation_delta)[0]
 
-
-plt.imshow(preprocess_frames(new_observation,observation),plt.cm.gray)
-
-preprocess_frames(new_observation,observation)
-preprocess_frames(new_observation,observation).shape
-
-# Modeling the Network
-
-# simple 2 layer model 
-# with 200 hidden units in first layer
-# and 1 sigmoid output
-inputs = keras.layers.Input(shape=(80,80))
-flattened_layer = keras.layers.Flatten()(inputs)
-full_connect_1 = keras.layers.Dense(units=200,activation='relu',use_bias=False,)(flattened_layer)
-sigmoid_output = keras.layers.Dense(1,activation='sigmoid',use_bias=False)(full_connect_1)
-policy_network_model = keras.models.Model(inputs=inputs,outputs=sigmoid_output)
-policy_network_model.summary()
-
-episode_reward = keras.layers.Input(shape=(1,),name='episode_reward')
-
-# Defining loss
-
-def m_loss(episode_reward):
-    def loss(y_true,y_pred):
-        # feed in y_true as actual action taken 
-        # if actual action was up, we feed 1 as y_true and otherwise 0
-        # y_pred is the network output(probablity of taking up action)
-        # note that we dont feed y_pred to network. keras computes it
-        
-        # first we clip y_pred between some values because log(0) and log(1) are undefined
-        tmp_pred = keras.layers.Lambda(lambda x: keras.backend.clip(x,0.05,0.95))(y_pred)
-        # we calculate log of probablity. y_pred is the probablity of taking up action
-        # note that y_true is 1 when we actually chose up, and 0 when we chose down
-        # this is probably similar to cross enthropy formula in keras, but here we write it manually to multiply it by the reward value
-        tmp_loss = keras.layers.Lambda(lambda x:-y_true*keras.backend.log(x)-(1-y_true)*(keras.backend.log(1-x)))(tmp_pred)
-        # multiply log of policy by reward
-        policy_loss=keras.layers.Multiply()([tmp_loss,episode_reward])
-        return policy_loss
-    return loss
-
-episode_reward = keras.layers.Input(shape=(1,),name='episode_reward')
-policy_network_train = keras.models.Model(inputs=[inputs,episode_reward],outputs=sigmoid_output)
-
-my_optimizer = keras.optimizers.RMSprop(lr=0.0001)
-policy_network_train.compile(optimizer=my_optimizer,loss=m_loss(episode_reward),)
-
-# Reward Engineering and why it is important
-def generate_episode(policy_network):
-    states_list = [] # shape = (x,80,80)
-    up_or_down_action_list=[] # 1 if we chose up. 0 if down
-    rewards_list=[]
-    network_output_list=[]
-    env=gym.make("Pong-v0")
-    observation = env.reset()
-    new_observation = observation
-    done = False
-    policy_output_list = []
-    
-    while done == False:
-    
-        processed_network_input = preprocess_frames(new_frame=new_observation,last_frame=observation)
-        states_list.append(processed_network_input)
-        reshaped_input = np.expand_dims(processed_network_input,axis=0) # x shape is (80,80) so we need similar reshape(x,(1,80,80))
-
-        up_probability = policy_network.predict(reshaped_input,batch_size=1)[0][0]
-        network_output_list.append(up_probability)
-        policy_output_list.append(up_probability)
-        actual_action = np.random.choice(a=[2,3],size=1,p=[up_probability,1-up_probability]) # 2 is up. 3 is down 
-        if actual_action==2:
-            up_or_down_action_list.append(1)
+        if np.random.uniform() < up_probability:
+            action = UP_ACTION
         else:
-            up_or_down_action_list.append(0)
-        
-        observation= new_observation
-        new_observation, reward, done, info = env.step(actual_action)
-        
-        rewards_list.append(reward)
-        
-        if done:
-            break
-            
-    env.close()
-    return states_list,up_or_down_action_list,rewards_list,network_output_list
+            action = DOWN_ACTION
 
-    states_list,up_or_down_action_list,rewards_list,network_output_list = generate_episode(policy_network_model)
+        observation, reward, episode_done, info = env.step(action)
+        observation = prepro(observation)
+        episode_reward_sum += reward
+        n_steps += 1
 
-    
+        tup = (observation_delta, action_dict[action], reward)
+        batch_state_action_reward_tuples.append(tup)
 
-print("length of states= "+str(len(states_list)))# this is the number of frames
-print("shape of each state="+str(states_list[0].shape))
-print("length of rewards= "+str(len(rewards_list)))
+        if reward == -1:
+            print("Round %d: %d time steps; lost..." % (round_n, n_steps))
+        elif reward == +1:
+            print("Round %d: %d time steps; won!" % (round_n, n_steps))
+        if reward != 0:
+            round_n += 1
+            n_steps = 0
 
+    print("Episode %d finished after %d rounds" % (episode_n, round_n))
 
-# lets see sample of policy output
-print(network_output_list[30:50]) 
+    # exponentially smoothed version of reward
+    if smoothed_reward is None:
+        smoothed_reward = episode_reward_sum
+    else:
+        smoothed_reward = smoothed_reward * 0.99 + episode_reward_sum * 0.01
+    print("Reward total was %.3f; discounted moving average of reward is %.3f" \
+        % (episode_reward_sum, smoothed_reward))
 
+    if episode_n % args.batch_size_episodes == 0:
+        states, actions, rewards = zip(*batch_state_action_reward_tuples)
+        rewards = discount_rewards(rewards, args.discount_factor)
+        rewards -= np.mean(rewards)
+        rewards /= np.std(rewards)
+        batch_state_action_reward_tuples = list(zip(states, actions, rewards))
+        network.train(batch_state_action_reward_tuples)
+        batch_state_action_reward_tuples = []
 
+    if episode_n % args.checkpoint_every_n_episodes == 0:
+        network.save_checkpoint()
 
-#lets see a sample what we actually did: 1 means we went up, 0 means down
-up_or_down_action_list[30:50]
-
-# lets see sample of rewards
-print(rewards_list[50:100]) 
-
-
-
-# lets see how many times we won through whole game:
-print("count win="+str(len(list(filter(lambda r: r>0,rewards_list)))))
-print("count lose="+str(len(list(filter(lambda r: r<0,rewards_list)))))
-print("count zero rewards="+str(len(list(filter(lambda r: r==0,rewards_list)))))
-
-plt.plot(rewards_list,'.')
-ax=plt.gca()
-ax.grid(True)
-
-def process_rewards(r_list):
-    reward_decay=0.99
-    tmp_r=0
-    rew=np.zeros_like(r_list,dtype=np.float32)
-    for i in range(len(r_list)-1,-1,-1):
-        if r_list[i]==0:
-            tmp_r=tmp_r*reward_decay
-            rew[i]=tmp_r
-        else: 
-            tmp_r = r_list[i]
-            rew[i]=tmp_r
-    rew -= np.mean(rew) # subtract by average
-    rew /= np.std(rew) # divide by std
-    return rew
-
-    
-
-# lets see what this gives us:
-plt.plot(process_rewards(rewards_list),'-')
-ax=plt.gca()
-ax.grid(True)
-
-# Example of simluation and training
-
-# first generate an episode:
-states_list,up_or_down_action_list,rewards_list,network_output_list = generate_episode(policy_network_model)
-
-print("length of states= "+str(len(states_list)))# this is the number of frames
-print("shape of each state="+str(states_list[0].shape))
-print("length of rewards= "+str(len(rewards_list)))
-
-#preprocess inputs for training: 
-    
-x=np.array(states_list)
-
-episode_reward=np.expand_dims(process_rewards(rewards_list),1)
-
-y_tmp = np.array(up_or_down_action_list) # 1 if we chose up, 0 if down
-y_true = np.expand_dims(y_tmp,1) # modify shape. this is neccassary for keras
-
-
-print("episode_reward.shape =",episode_reward.shape)
-print("x.shape =",x.shape)
-print("y_true.shape =",y_true.shape)
-
-#  fit the model with inputs and outputs.
-policy_network_train.fit(x=[x,episode_reward],y=y_true)
-
-
-# Training the network
-
-# we define a helper function to create a batch of simulations
-# and after the batch simulations, preprocess data and fit the network
-def generate_episode_batches_and_train_network(n_batches=10):
-    env = gym.make('Pong-v0')
-    batch_state_list=[]
-    batch_up_or_down_action_list=[]
-    batch_rewards_list=[]
-    batch_network_output_list=[]
-    for i in range(n_batches):
-        states_list,up_or_down_action_list,rewards_list,network_output_list = generate_episode(policy_network_model)   
-        batch_state_list.extend(states_list[15:])
-        batch_network_output_list.extend(network_output_list[15:])
-        batch_up_or_down_action_list.extend(up_or_down_action_list[15:])
-        batch_rewards_list.extend(rewards_list[15:])
-    
-    episode_reward=np.expand_dims(process_rewards(batch_rewards_list),1)
-    x=np.array(batch_state_list)
-    y_tmp = np.array(batch_up_or_down_action_list)
-    y_true = np.expand_dims(y_tmp,1)
-    policy_network_train.fit(x=[x,episode_reward],y=y_true)
-
-    return batch_state_list,batch_up_or_down_action_list,batch_rewards_list,batch_network_output_list
-
-train_n_times = 21 # for actual training, about 5000 may be a good start. 
-for i in range(train_n_times):
-    states_list,up_or_down_action_list,rewards_list,network_output_list=generate_episode_batches_and_train_network(10)
-    if i%10==0:
-        print("i="+str(i))
-        rr=np.array(rewards_list)
-        # i keep how many times we won in batch. you can use log more details more frequently
-        print('count win='+str(len(rr[rr>0]))) 
-        policy_network_model.save("policy_network_model_simple.h5")
-        policy_network_model.save("policy_network_model_simple"+str(i)+".h5")
-        with open('rews_model_simple.txt','a') as f_rew:
-            f_rew.write("i="+str(i)+'       reward= '+str(len(rr[rr > 0])))
-            f_rew.write("\n")
-
-#  Playing the Trained Network
-def play_and_show_episode(policy_network):
-    env = gym.make('Pong-v0')
-    done=False
-    observation = env.reset()
-    new_observation = observation
-    while done==False:
-        time.sleep(1/80)
-        
-        processed_network_input = preprocess_frames(new_frame=new_observation,last_frame=observation)
-        reshaped_input = np.expand_dims(processed_network_input,axis=0) # x shape is (80,80) so we need similar reshape(x,(1,80,80))
-
-        up_probability = policy_network.predict(reshaped_input,batch_size=1)[0][0]
-        actual_action = np.random.choice(a=[2,3],size=1,p=[up_probability,1-up_probability])
-        
-        env.render()
-        
-        observation= new_observation
-        new_observation, reward, done, info = env.step(actual_action)
-        if reward!=0:
-            print(reward)
-        if done:
-            break
-        
-    env.close()
-
-    # Loading model from file
-
-policy_network_model=keras.models.load_model("trained_simple_model_3300.h5")
-policy_network_model.summary()
-
-episode_reward = keras.layers.Input(shape=(1,),name='episode_reward')
-
-def m_loss(episode_reward):
-    def loss(y_true,y_pred):
-        # feed in y_true as actual action taken
-        # loss = reward*(-actual*np.log(y_pred)-(1-actual)*np.log(1-y_pred)))
-        
-        tmp_pred = keras.layers.Lambda(lambda x: keras.backend.clip(x,0.05,0.95))(y_pred) # we could also do gradient clipping
-        tmp_loss = keras.layers.Lambda(lambda x:-y_true*keras.backend.log(x)-(1-y_true)*(keras.backend.log(1-x)))(tmp_pred)
-        # put reward in effect
-        policy_loss=keras.layers.Multiply()([tmp_loss,episode_reward])
-        
-        return policy_loss
-    return loss
-
-policy_network_train = keras.models.Model(inputs=[policy_network_model.input,episode_reward],outputs=policy_network_model.output)
-my_optimizer = keras.optimizers.RMSprop(lr=0.0001)
-policy_network_train.compile(optimizer=my_optimizer,loss=m_loss(episode_reward),)
-
-
-play_and_show_episode(policy_network_model)
-
-# Using Convolutional Model
-
-policy_network_model=keras.models.load_model("trained_conv_model.h5")
-policy_network_model.summary()
-
-play_and_show_episode(policy_network_model)
+    episode_n += 1
